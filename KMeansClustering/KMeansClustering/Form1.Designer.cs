ae6fef2af6eec6659287b18feb83298ec1530baa
TranSinh10 <80934375+TranSinh10@users.noreply.github.com>
2023-04-17 14:06:41 +0700

    KMeans

---
+++ KMeansClustering/KMeansClustering/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace KMeansClustering
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            loadDataButton = new Button();
+            clusterButton = new Button();
+            dataGridView1 = new DataGridView();
+            numClustersTextBox = new TextBox();
+            plotView1 = new OxyPlot.WindowsForms.PlotView();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // loadDataButton
+            // 
+            loadDataButton.BackColor = Color.FromArgb(255, 128, 128);
+            loadDataButton.Location = new Point(33, 43);
+            loadDataButton.Name = "loadDataButton";
+            loadDataButton.Size = new Size(75, 23);
+            loadDataButton.TabIndex = 0;
+            loadDataButton.Text = "Load Data";
+            loadDataButton.UseVisualStyleBackColor = false;
+            loadDataButton.Click += loadDataButton_Click;
+            // 
+            // clusterButton
+            // 
+            clusterButton.BackColor = Color.FromArgb(255, 128, 128);
+            clusterButton.Location = new Point(33, 107);
+            clusterButton.Name = "clusterButton";
+            clusterButton.Size = new Size(75, 23);
+            clusterButton.TabIndex = 1;
+            clusterButton.Text = "Clustering";
+            clusterButton.UseVisualStyleBackColor = false;
+            clusterButton.Click += clusterButton_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(42, 267);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(728, 171);
+            dataGridView1.TabIndex = 3;
+            // 
+            // numClustersTextBox
+            // 
+            numClustersTextBox.Location = new Point(123, 44);
+            numClustersTextBox.Name = "numClustersTextBox";
+            numClustersTextBox.Size = new Size(78, 23);
+            numClustersTextBox.TabIndex = 4;
+            numClustersTextBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // plotView1
+            // 
+            plotView1.BackColor = Color.White;
+            plotView1.Location = new Point(287, 43);
+            plotView1.Name = "plotView1";
+            plotView1.PanCursor = Cursors.Hand;
+            plotView1.Size = new Size(483, 218);
+            plotView1.TabIndex = 5;
+            plotView1.Text = "plotView1";
+            plotView1.ZoomHorizontalCursor = Cursors.SizeWE;
+            plotView1.ZoomRectangleCursor = Cursors.SizeNWSE;
+            plotView1.ZoomVerticalCursor = Cursors.SizeNS;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(123, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 25);
+            label1.TabIndex = 6;
+            label1.Text = "Số cụm";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(473, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(173, 25);
+            label2.TabIndex = 7;
+            label2.Text = "Kết quả phân cụm";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = Color.FromArgb(255, 224, 192);
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(plotView1);
+            Controls.Add(numClustersTextBox);
+            Controls.Add(dataGridView1);
+            Controls.Add(clusterButton);
+            Controls.Add(loadDataButton);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button loadDataButton;
+        private Button clusterButton;
+        private DataGridView dataGridView1;
+        private TextBox numClustersTextBox;
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ KMeansClustering/KMeansClustering/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System.Data;
+using System.Diagnostics.Metrics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+
+namespace KMeansClustering
+{
+    public partial class Form1 : Form
+    {
+        private DataTable customerTable;
+        private List<Customer> customers;
+        private List<Cluster> clusterList;
+        public Form1()
+        {
+            InitializeComponent();
+            numClustersTextBox.Text = "3";
+        }
+        public class Customer
+        {
+            public int Id { get; set; }
+            public int Gender { get; set; }
+            public int Age { get; set; }
+            public int Income { get; set; }
+            public int Spending { get; set; }
+            public Cluster Cluster { get; set; }
+        }
+
+        public class Cluster
+        {
+            public Customer Centroid { get; set; }
+            public List<Customer> Customers { get; set; }
+
+            public Cluster()
+            {
+                Customers = new List<Customer>();
+            }
+        }
+        private List<Customer> LoadDataFromCsv(string filename)
+        {
+            List<Customer> customers = new List<Customer>();
+
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+
+                    // Tạo đối tượng Customer từ các giá trị đọc được
+                    Customer customer = new Customer
+                    {
+                        Id = int.Parse(values[0]),
+                        Gender = int.Parse(values[1]),
+                        Age = int.Parse(values[2]),
+                        Income = int.Parse(values[3]),
+                        Spending = int.Parse(values[4])
+                    };
+
+                    // Thêm khách hàng vào danh sách
+                    customers.Add(customer);
+                }
+            }
+
+            return customers;
+        }
+        private void DisplayData(List<Customer> customers, DataGridView dataGridView1)
+        {
+            dataGridView1.DataSource = customers;
+        }
+
+        private void loadDataButton_Click(object sender, EventArgs e)
+        {
+            string filePath = "E:\\Đồ án tốt nghiệp\\Dữ liệu\\Mall_Customer.csv";
+            customers = LoadDataFromCsv(filePath);
+            DisplayData(customers, dataGridView1);
+        }
+        private double GetEuclideanDistance(Customer customer, Customer centroid)
+        {
+            double distance = 0;
+            distance += Math.Pow(customer.Income - centroid.Income, 2);
+            distance += Math.Pow(customer.Spending - centroid.Spending, 2);
+            return Math.Sqrt(distance);
+        }
+        private void clusterButton_Click(object sender, EventArgs e)
+        {
+            if (customers == null)
+            {
+                MessageBox.Show("Please load data first.");
+                return;
+            }
+            int k = int.Parse(numClustersTextBox.Text);
+
+
+            // Khởi tạo K centroid ngẫu nhiên
+            clusterList = new List<Cluster>();
+            Random rand = new Random();
+            for (int i = 0; i < k; i++)
+            {
+                int index = rand.Next(customers.Count);
+                Cluster cluster = new Cluster { Centroid = customers[index] };
+                clusterList.Add(cluster);
+            }
+
+            // Thực hiện phân cụm K-Means
+            bool isClusterChanged = true;
+            int iteration = 0;
+            while (isClusterChanged && iteration < 1000)
+            {
+                isClusterChanged = false;
+                iteration++;
+
+                // Gán mỗi dòng dữ liệu vào cụm gần nhất
+                foreach (Customer customer in customers)
+                {
+                    double minDistance = double.MaxValue;
+                    Cluster nearestCluster = null;
+                    foreach (Cluster cluster in clusterList)
+                    {
+                        double distance = GetEuclideanDistance(customer, cluster.Centroid);
+                        if (distance < minDistance)
+                        {
+                            minDistance = distance;
+                            nearestCluster = cluster;
+                        }
+                    }
+                    if (nearestCluster != customer.Cluster)
+                    {
+                        isClusterChanged = true;
+                        customer.Cluster?.Customers.Remove(customer);
+                        nearestCluster.Customers.Add(customer);
+                        customer.Cluster = nearestCluster;
+                    }
+                }
+
+                // Cập nhật vị trí centroid
+                foreach (Cluster cluster in clusterList)
+                {
+                    if (cluster.Customers.Any())
+                    {
+                        double sumAge = cluster.Customers.Sum(c => c.Age);
+                        double sumIncome = cluster.Customers.Sum(c => c.Income);
+                        double sumSpending = cluster.Customers.Sum(c => c.Spending);
+                        int count = cluster.Customers.Count;
+                        Customer newCentroid = new Customer
+                        {
+                            Id = cluster.Centroid.Id,
+                            Gender = cluster.Centroid.Gender,
+                            Age = cluster.Centroid.Gender,
+                            Income = (int)Math.Round(sumIncome / count),
+                            Spending = (int)Math.Round(sumSpending / count)
+                        };
+                        cluster.Centroid = newCentroid;
+                    }
+                }
+            }
+            // Hiển thị kết quả phân cụm lên biểu đồ
+            PlotModel plotModel = new PlotModel();
+            CategoryAxis xAxis = new CategoryAxis { Position = AxisPosition.Bottom };
+            LinearAxis yAxis = new LinearAxis { Position = AxisPosition.Left };
+            plotModel.Axes.Add(xAxis);
+            plotModel.Axes.Add(yAxis);
+
+            for (int i = 0; i < clusterList.Count; i++)
+            {
+                ScatterSeries series = new ScatterSeries { Title = $"Cluster {i + 1}" };
+                foreach (Customer customer in clusterList[i].Customers)
+                {
+                    series.Points.Add(new ScatterPoint(customer.Income, customer.Spending, 3));
+                }
+                plotModel.Series.Add(series);
+            }
+            for (int i = 0; i < clusterList.Count; i++)
+            {
+                Cluster cluster = clusterList[i];
+                ScatterSeries centroidSeries = new ScatterSeries { Title = $"Centroid {i + 1}" };
+                centroidSeries.MarkerFill = OxyColor.FromRgb(0, 100, 100);
+                centroidSeries.Points.Add(new ScatterPoint(cluster.Centroid.Income, cluster.Centroid.Spending, 5));
+                plotModel.Series.Add(centroidSeries);
+            }
+
+            plotView1.Model = plotModel;
+        }
+    }
+}
